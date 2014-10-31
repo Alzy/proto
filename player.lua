b2c44ce77ecc8bcb62e2ed54c74272407e5c3fba
Alzy <gonzalezalfredo101@gmail.com>
2014-10-31 00:56:56 -0700

    update1.02
    fixed jumps
    speed changes
    updated documentations
    acquired animation library

--- player.lua
+++ player.lua
@@ -15,14 +15,14 @@ function player.load( )
 	player.accY = 0
 
 	-- [Player Stats]
-	player.speed = 255
+	player.speed = 350
 	player.hp = 1000
 	player.energy = 5000
 	player.attack = 2 -- out of 3. where 1 is weak, 2 is well average, and 3 is strong.
 	player.defense = 2 -- ditto^
 
 	-- [ Temp constants and variables (for dev)]
-	player.gravity = 120
+	player.gravity = 2500
 
 	--player.hitbox_x = player.x + 12
 	--player.hitbox_y = player.y + 12
@@ -32,6 +32,12 @@ end
 function player.draw( )
 	player.sprite = player.sprite
 	love.graphics.draw ( player.sprite, player.x, player.y )
+
+	love.graphics.printf( player.state, 10, 10, 150, 'left' )
+	love.graphics.printf( "facing:  " .. player.facing, 300, 10, 150, 'left' )
+	love.graphics.printf( "velocity:  " .. player.velY, 10, 30, 550, 'left' )
+	love.graphics.printf( "acceleration:  " .. player.accY, 10, 50, 550, 'left' )
+	love.graphics.printf( "speed:  " .. player.speed, 10, 70, 550, 'left' )
 	-- player.hitbox:draw("fill")
 end
 
@@ -39,35 +45,60 @@ function player.update( dt )
 	if love.keyboard.isDown( "up" ) then
 		if player.state ~= 'jumping' then
 			player.state = 'jumping'
-			player.velY = 225
-		elseif player.state == 'jumping' then
-			-- jump
-
-			--apply gravity
-			player.accY = player.accY - ( player.gravity * dt )
-
-			--update velocity
-			player.velY = player.velY + ( player.accY * dt )
-
-			--update position
-			player.y = player.y - player.velY * dt
-
-			if player.y >= 215 then
-				player.state = 'idle'
-			end
-
-		end 
+			player.velY = 450
+		end
 	end
 	if love.keyboard.isDown( "down" ) then
-		--
+		player.velY = player.velY - 50
 	end
 	if love.keyboard.isDown( "left" ) then
 		player.x = player.x - ( player.speed * dt )
-		player.facing = "left"
+		if player.state ~= "jumping" then
+			player.facing = "left"
+		end
+
 	end
 	if love.keyboard.isDown( "right" ) then
 		player.x = player.x + ( player.speed * dt )
-		player.facing = "right"
+		if player.state ~= "jumping" then	
+			player.facing = "right"
+		end
 	end
 	--player.hitbox:moveTo( player.hitbox_x, player.hitbox_y)
+
+	-- [Plater State Resolve]
+	if player.state == "idle" then
+			player.velY = 0
+			player.accY = 0
+			player.speed = 350
+	end
+
+	if player.state == 'jumping' then
+		-- jump
+
+		--apply gravity
+		player.accY = player.accY - ( player.gravity * dt )
+
+		--update velocity
+		player.velY = player.velY + ( player.accY * dt )
+
+		--update position
+		player.y = player.y - player.velY * dt
+
+		if player.y >= 215 then
+			player.state = 'idle'
+			player.velY = 0
+			player.accY = 0
+			player.speed = 350
+		end
+
+		-- back jump resistance
+		if love.keyboard.isDown( "left" ) and player.facing == "right" then
+			player.speed = 200
+		end
+		if love.keyboard.isDown( "right" ) and player.facing == "left" then
+			player.speed = 200
+		end
+
+	end 
 end
