7af46d0aff9c36e949f6193827f29c4cc206e327
Alzy <gonzalezalfredo101@gmail.com>
2014-11-14 17:36:21 -0800

    update 1.087
    new movement mechanic in works.

--- scratch.lua
+++ scratch.lua
@@ -61,7 +61,64 @@ function player:keyboard()
 		end
 
 	end 
-
-
 	-- body
+end
+
+
+
+
+function player:movement( dt )
+	self.xInput =  self.speed * joystick:getAxis(1)
+	-- UP
+		if joystick:getAxis(2) < -0.79 then
+			if self.state ~= 'jumping' and self.state ~= 'falling' then
+				self.state = 'jumping'
+				self.velY = tonumber(self.charSheetArray["jumpVel"])  -- must be variable per character
+			end
+		end
+	-- DOWN
+		if joystick:getAxis(2) > 0.65 then
+			self.velY = self.velY - 55 -- must be the product of delta time.
+		end
+
+	-- LEFT AND RIGHT
+		local joystickXpos = joystick:getAxis(1)
+	-- POSITION 1 (walk)
+		if math.abs(joystickXpos) > 0.17 and math.abs(joystickXpos) < 0.35 then
+			-- if on the ground and accX == 0 & velX == 0
+			self.x = self.x + ( self.xInput * dt )
+	-- POSTITION 2 (run)
+		elseif math.abs(joystickXpos) >= 0.35 and math.abs(joystickXpos < 0.85) then
+			if self.velX == 0 and accX == 0 then self.velX = self.xInput end
+			-- on ground
+			self.accX = self.accX + ( self.xInput * dt )
+	-- POSITION 3 (sprint)
+		elseif math.abs(joystickXpos) >= 0.85 then
+			-- on ground
+			self.accX = self.accX + ( self.xInput * dt )
+		end
+
+	-- limit speed and acceleration
+		if self.velX > self.speed then self.velX = self.speed; self.accX = 0 end
+		if self.velX < self.speed * -1 then self.velX = self.speed * -1; self.accX = 0 end
+
+
+	-- STOP PLAYER AT NEUTRAL POSITION.
+		if math.abs(joystickXpos) < 0.35  and self.velX ~= 0 and self.state ~= "jumping" and self.state ~= "falling" then
+			--bring player to hault.
+			if math.abs(self.velX) <= self.speed * 0.2 then
+				self.velX = 0
+				self.accX = 0
+			end
+
+			if math.abs(self.velX) > 0 then
+				self.accX = self.accX - ( (self.velX * 20) * dt )
+			end
+
+		end
+
+	-- update velocity
+	self.velX = self.velX + ( self.accX * dt )
+	-- update player x location
+	self.x = self.x + ( self.velX * dt )
 end
